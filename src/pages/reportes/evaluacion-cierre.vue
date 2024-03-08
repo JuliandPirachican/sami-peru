a373b2359b4cea300913a3d4ab1fe1ff56152318
YonathanDupree <yonathan.galindo@dupree.com.pe>
2024-03-08 14:39:52 -0500

    Se quita filtro para obtener zona

--- src/pages/reportes/evaluacion-cierre.vue
+++ src/pages/reportes/evaluacion-cierre.vue
@@ -386,11 +386,7 @@ const obtenerZona = async () => {
 
     const { data } = await $api(`/api/comun/v1/zonas`, {
       method: "get",
-      query: {
-        codigo: userData.codi_perf,
-      },
     })
-    
 
     const itemZona = data.data_glob
     
