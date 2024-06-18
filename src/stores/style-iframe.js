a3fd64614edabe747b1b0ac2859d6c7141e426a5
JuliandPirachican <151785462+JuliandPirachican@users.noreply.github.com>
2024-06-18 14:34:30 -0500

    cambiio de funcion de estilos
    Se coloca hoja de estilos para los modulos de cgis enmbebidos

--- src/stores/style-iframe.js
+++ src/stores/style-iframe.js
@@ -1,6 +1,32 @@
-export function style_iframe() {
+function apply_class_input(contentiframe){
+    var inputs=contentiframe.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+      inputs[i].className += " form-control";
+    }
+    return inputs
+  }
+
+  function apply_class_table(contentiframe){
+    var table=contentiframe.getElementsByTagName("table");
+    for (var i = 0; i < table.length; i++) {
+      table[i].className += " table table-borderless  table-responsive";
+    }
+
+    return table
+  }
+ 
+  function apply_class_img_btn(contentiframe){
+    var img=contentiframe.getElementsByTagName("img");
+    for (var i = 0; i < img.length; i++) {
+      img[i].className += "btn btn-warning";
+    }
+    return img
+  }
+
+export function style_iframe_cgis() {
     console.log("dentro del archivo de estilo")
     var iframe = document.getElementById("iframe_option");
+    iframe.style.height="500px";
     iframe.onload = function () {
       var contentiframe = iframe.contentDocument;
       console.log(contentiframe);
@@ -9,13 +35,26 @@ export function style_iframe() {
       stylebootstrap.rel = "stylesheet";
       stylebootstrap.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css";
       contentiframe.head.appendChild(stylebootstrap);
-
+    
       var style_def=contentiframe.getElementsByTagName("link")[0];
-      style_def.parentNode.removeChild(style_def)
-  
+      style_def.parentNode.removeChild(style_def);
+
       var scriptbootstrap = contentiframe.createElement("script");
       scriptbootstrap.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js";
       contentiframe.body.appendChild(scriptbootstrap);
+
+      var title=contentiframe.getElementsByClassName("x-titulo");
+      title.className+="text-center";
+      
+      apply_class_input(contentiframe);
+      apply_class_table(contentiframe);
+      apply_class_img_btn(contentiframe);
+
+     
     };
   }
+
+
+
+  
   
