e97a3182ce6af94789c70952e4ab6dc2c4d37af6
JhonathanGaravito <jhonathan.garavito@azzorti.co>
2024-06-18 16:51:03 -0500

    estilos indicadores
    Se realiza los estilos de los indicadores de gestion

--- src/pages/reportes/indicadores-gestion.vue
+++ src/pages/reportes/indicadores-gestion.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useAppStore } from '@/stores/app'
+import { style_iframe_cgis } from '@/stores/style-iframe'
 
 definePage({
   meta: { 
@@ -128,6 +129,7 @@ const onLimpiar = () => {
 onMounted(() => {
   appStore.titulo(`Reportes / Indicadores de gestión`) 
   onGenerar() 
+  style_iframe_cgis() 
 }) 
 </script>
 
