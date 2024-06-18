5e890b7f9989f235e8c72d9a85079b3dd3889be7
JuliandPirachican <151785462+JuliandPirachican@users.noreply.github.com>
2024-06-18 16:04:29 -0500

    subir estilos
    se suben estilos para cgis y heigth en general

--- src/stores/style-iframe.js
+++ src/stores/style-iframe.js
@@ -8,9 +8,15 @@ function apply_class_input(contentiframe){
 
   function apply_class_table(contentiframe){
     var table=contentiframe.getElementsByTagName("table");
+    var tdelements=contentiframe.querySelectorAll("table td");
     for (var i = 0; i < table.length; i++) {
-      table[i].className += " table table-borderless  table-responsive";
+        table[i].className += " table table-borderless  table-responsive text-center ";
     }
+
+    tdelements.forEach(td => {
+        td.removeAttribute('align');
+        td.removeAttribute('width');
+      });
 
     return table
   }
@@ -28,27 +34,27 @@ export function style_iframe_cgis() {
     var iframe = document.getElementById("iframe_option");
     iframe.style.height="500px";
     iframe.onload = function () {
-      var contentiframe = iframe.contentDocument;
-      console.log(contentiframe);
+    //   var contentiframe = iframe.contentDocument;
+    //   console.log(contentiframe);
   
-      var stylebootstrap = contentiframe.createElement("link");
-      stylebootstrap.rel = "stylesheet";
-      stylebootstrap.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css";
-      contentiframe.head.appendChild(stylebootstrap);
+    //   var stylebootstrap = contentiframe.createElement("link");
+    //   stylebootstrap.rel = "stylesheet";
+    //   stylebootstrap.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css";
+    //   contentiframe.head.appendChild(stylebootstrap);
     
-      var style_def=contentiframe.getElementsByTagName("link")[0];
-      style_def.parentNode.removeChild(style_def);
+    //   var style_def=contentiframe.getElementsByTagName("link")[0];
+    //   style_def.parentNode.removeChild(style_def);
 
-      var scriptbootstrap = contentiframe.createElement("script");
-      scriptbootstrap.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js";
-      contentiframe.body.appendChild(scriptbootstrap);
+    //   var scriptbootstrap = contentiframe.createElement("script");
+    //   scriptbootstrap.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js";
+    //   contentiframe.body.appendChild(scriptbootstrap);
 
-      var title=contentiframe.getElementsByClassName("x-titulo");
-      title.className+="text-center";
+    //   var title=contentiframe.getElementsByClassName("x-titulo");
+    //   title.className+="text-center";
       
-      apply_class_input(contentiframe);
-      apply_class_table(contentiframe);
-      apply_class_img_btn(contentiframe);
+    //   apply_class_input(contentiframe);
+    //   apply_class_table(contentiframe);
+    //   apply_class_img_btn(contentiframe);
 
      
     };
