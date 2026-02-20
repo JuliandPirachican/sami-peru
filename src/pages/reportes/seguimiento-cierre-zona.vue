d014f5417c43b0f3dd09814c2996247742eacdc6
JuliandPirachican <151785462+JuliandPirachican@users.noreply.github.com>
2026-02-20 11:24:42 -0500

    ajuste zonas perfil
    se hace un ajuste para colocar un filtro que dependiendo de las persona que ingrese buscara la zona correspondiente a su perfil GZ,GR,L o admin

--- src/pages/reportes/seguimiento-cierre-zona.vue
+++ src/pages/reportes/seguimiento-cierre-zona.vue
@@ -1,7 +1,9 @@
 <!-- eslint-disable camelcase -->
 <script setup>
 import { useAppStore } from '@/stores/app';
+import { EncryptStorage } from 'encrypt-storage';
 import JqxGrid from 'jqwidgets-scripts/jqwidgets-vue3/vue_jqxgrid.vue';
+
 
 definePage({
   meta: {
@@ -10,7 +12,13 @@ definePage({
   },
 })
 
-const appStore = useAppStore()
+const appStore = useAppStore();
+
+const encryptStorage = new EncryptStorage('AZZORTI-SAMI', {
+  storageType: 'localStorage',
+})
+
+const userData = encryptStorage.getItem('userData')
 
 const formulario = ref({
   campana: null,
@@ -2884,6 +2892,9 @@ const obtenerZona = async () => {
 
     const { data } = await $api(`/api/comun/v1/zonas`, {
       method: "get",
+      query: {
+        codigo: userData.codi_perf,
+      },
     })
     
     const itemZona = data.data_glob
