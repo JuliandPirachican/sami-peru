c17a3601bab9035246f779dad59aa515f769e372
JuliandPirachican <151785462+JuliandPirachican@users.noreply.github.com>
2026-05-20 09:11:23 -0500

    cambia nombre opcion
    se cambia el nombre de la opcion nombramiento de lideres a activa/desactiva lideres por peticion de usuario funcional

--- src/navigation/vertical/procesos.js
+++ src/navigation/vertical/procesos.js
@@ -125,7 +125,7 @@ export default [
     action: 'colombia/pre_pedido',
   },
   { 
-    title: 'Nombramiento De Lideres', 
+    title: 'Activa/desactiva lideres', 
     to: 'procesos-gest-lide-tabl-lide',
     icon: { icon: 'tabler-world-up' },
     subject: 'colombia/gest_lide_tabl_lide',  
